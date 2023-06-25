2528095a57480430a04aeadef9cd629190ab593a
Daniel <dankoskdev@gmail.com>
2023-06-25 17:38:02 +0200

    Logout fetch implemented

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,9 +4,6 @@ import Register from "./Register";
 import './Navbar.css';
 
 
-
-
-
 class Navbar extends React.Component  {
     constructor() {
         super();
@@ -17,8 +14,6 @@ class Navbar extends React.Component  {
         }
         this.setSearchDistance = this.setSearchDistance.bind(this)
     }
-
-
  handleLoginActive(){
         if (this.state.login){
             this.setState({login:false})
@@ -40,9 +35,29 @@ class Navbar extends React.Component  {
         this.props.setSearchDistance(searchDistance)
     }
     handleLogout() {
-        localStorage.removeItem("jwtToken")
-        window.location.reload()
-    }
+        const token = localStorage.getItem("jwtToken");
+        fetch("http://localhost:8081/api/auth/logout", {
+            method: "POST",
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("jwtToken")}`,
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ token }),
+        })
+            .then((response) => {
+                if (response.ok) {
+                    localStorage.removeItem("jwtToken");
+                    window.location.reload();
+                } else {
+                    throw new Error("Wystąpił błąd podczas wylogowywania");
+                }
+            })
+            .catch((err) => {
+                console.log(err.message, err);
+            });
+    };
+
+
   render() {
 
       return (
